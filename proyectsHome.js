b4af838573b2ff30d4c9bfa59125b51c2db2d341
Jimena Hernández <her21199@uvg.edu.gt>
2023-11-02 11:33:12 -0600

    Update proyectsHome.js

--- proyectsHome.js
+++ proyectsHome.js
@@ -1,5 +1,42 @@
 // projectData.js
 export const projectsHome = [
+  {
+    id : "6",
+    title: 'Pour Home/ Paleta paints',
+    imageUrl: '/images/proyectsImages/06.png',
+    description: `This makes your house look like a star. Like a celebrity...or just like home. ;) 
+    Thnx @pinturaspaleta for 25 years of wonderful creative partnership.
+    #pinturas
+    #paleta
+    #decor
+    #homedecor
+    #fashion
+    #hautecouture
+    #beautiful
+    #designculture
+    #designculturecompany
+    #guatemala`
+  },
+  {
+      id : "7",
+      title: 'Fridas',
+      imageUrl: '/images/proyectsImages/07.png',
+      description: `
+      Re design for Mexican restaurant Fridas
+      Fridas
+      Redesign for one of the most well-loved restaurants in Guatemala City. The graphic line for this Mexican restaurant aims to celebrate both the traditional and modern aspects of Mexico. It acknowledges Mexico's rich history, while also showcasing its innovation, urban flair, and street style.`
+
+    },
+  {
+      id : "8",
+      title: 'Rebranding/Porta Hotels',
+      imageUrl: '/images/proyectsImages/08.png',
+      description: `
+      Rebranding for Porta Hotels
+      Porta Hotels
+      Rebranding for Porta Hotels, a group of hotels in Guatemala, Honduras, and Costa Rica. The new identity is based on the idea of "the art of hospitality," and it aims to communicate the brand's values of warmth, elegance, and authenticity.`
+
+    },
     {
       id : "1",
       title: 'We like balloons',
@@ -38,42 +75,5 @@ export const projectsHome = [
         title: 'Coca Cola/ Special Shapes',
         imageUrl: '/images/proyectsImages/05.jpg',
       },
-      {
-        id : "6",
-        title: 'Pour Home/ Paleta paints',
-        imageUrl: '/images/proyectsImages/06.png',
-        description: `This makes your house look like a star. Like a celebrity...or just like home. ;) 
-        Thnx @pinturaspaleta for 25 years of wonderful creative partnership.
-        #pinturas
-        #paleta
-        #decor
-        #homedecor
-        #fashion
-        #hautecouture
-        #beautiful
-        #designculture
-        #designculturecompany
-        #guatemala`
-      },
-      {
-          id : "7",
-          title: 'Fridas',
-          imageUrl: '/images/proyectsImages/07.png',
-          description: `
-          Re design for Mexican restaurant Fridas
-          Fridas
-          Redesign for one of the most well-loved restaurants in Guatemala City. The graphic line for this Mexican restaurant aims to celebrate both the traditional and modern aspects of Mexico. It acknowledges Mexico's rich history, while also showcasing its innovation, urban flair, and street style.`
-  
-        },
-      {
-          id : "8",
-          title: 'Rebranding/Porta Hotels',
-          imageUrl: '/images/proyectsImages/08.png',
-          description: `
-          Rebranding for Porta Hotels
-          Porta Hotels
-          Rebranding for Porta Hotels, a group of hotels in Guatemala, Honduras, and Costa Rica. The new identity is based on the idea of "the art of hospitality," and it aims to communicate the brand's values of warmth, elegance, and authenticity.`
-
-        },
 
   ];
